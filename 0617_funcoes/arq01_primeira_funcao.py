e26983804a2f6f6dcc01abbcf7ea2a9c5d75991a
MuriloTLH <omurilox159@gmail.com>
2026-06-25 22:22:49 -0300

    aula do dia 25/06

--- 0617_funcoes/arq01_primeira_funcao.py
+++ 0617_funcoes/arq01_primeira_funcao.py
@@ -1,5 +1,8 @@
-def nome_da_funcao (parametro1, parametro2):
+def somar_valores (parametro1, parametro2):
     """Docstring: descreve o que a função faz"""
     # Corpo da função
-    resultado = parametro1 = parametro2
+    resultado = parametro1 + parametro2
     return resultado
+
+soma = somar_valores(2,3)
+print(soma)
--- Logica_Python/README.md
+++
@@ -1,2 +0,0 @@
-# Logica_Python
-Disciplina do curso Tec. em desenvolvimento de sistema do SENAI
