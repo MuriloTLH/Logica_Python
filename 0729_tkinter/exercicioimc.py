05f1bf85e30d808a5aff1a446e3508a3ef920339
MuriloTLH <omurilox159@gmail.com>
2026-07-31 21:29:39 -0300

    final aula dia 29/07

--- 0729_tkinter/exercicioimc.py
+++ 0729_tkinter/exercicioimc.py
@@ -5,6 +5,22 @@ root = tk.Tk()
 root.title("SENAI-Sistemas")
 root.geometry("400x300")
 
+def calcular_imc():
+    valor_peso = float(peso_e.get())
+    valor_altura = float(altura_e.get())
+    
+    resultado = valor_peso / (valor_altura ** 2)
+
+    if resultado <= 18.5:
+        exibe_class.config(text="Abaixo do peso")
+    elif resultado <= 24.9:
+        exibe_class.config(text="Saudável")
+    elif resultado <= 29.9:
+        exibe_class.config(text="Sobrepeso")
+    elif resultado >= 30:
+        exibe_class.config(text="Obesidade")
+    res.config(text=f"IMC: {resultado:.1f}")
+
 peso = tk.Label(root, text="peso[Kg]")
 peso .pack()
 
@@ -12,25 +28,25 @@ peso_e = tk.Entry(root)
 peso_e.bind("<<Return>>")
 peso_e.pack()
 
-
 altura = tk.Label(root, text="Altura(m)")
-altura.pack(
-    # expand=True
-    )
+altura.pack()
 
 altura_e = tk.Entry(root)
 altura_e.bind("<<Return>>")
 altura_e.pack()
 
-# def calcular_peso():
-    
-
-    
 calcular = tk.Button(
     root,
-    text="calcular"
-    # command=
+    text="calcular",
+    command=calcular_imc
     )
 calcular.pack()
+
+res = tk.Label(root, text="Preencha os campos")
+exibe_class = tk.Label(root, text=" ")
+
+res.pack()
+exibe_class.pack()
+
 
 root.mainloop()
